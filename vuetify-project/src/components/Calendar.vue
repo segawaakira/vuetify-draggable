30c74305c432d84a67dea44665b88f6979a7fd63
segawa <>
2023-12-05 14:11:26 +0900

    add fullcalendar

---
+++ vuetify-project/src/components/Calendar.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+const currentView = ref("dayGridMonth"); // 初期表示を月表示に設定
+
+const calendarOptions = ref({
+  plugins: [dayGridPlugin, timeGridPlugin],
+  initialView: currentView.value,
+  // 他のカレンダーオプションをここに追加
+});
+
+// 表示モードを設定する関数
+const setCurrentView = (view: any) => {
+  currentView.value = view;
+};
+</script>
+
+<template>
+  <div>
+    <button @click="setCurrentView('dayGridMonth')">月表示</button>
+    <button @click="setCurrentView('timeGridWeek')">週表示</button>
+    <FullCalendar :options="calendarOptions" />
+  </div>
+</template>
--- vuetify-project/src/views/Home.vue
+++ vuetify-project/src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+  <Calendar />
 </template>
 
 <script lang="ts" setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import Calendar from "@/components/Calendar.vue";
 </script>
